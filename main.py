3af483052592e602125129892f08e8dc0ab36da3
lingen <*\(^_^)/*>
2022-08-05 15:19:43 +0800

    缺失依赖

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from util.index import Common
 from server.connect.dao import Table
 
 from db import engine
-from sqlmodel import create_engine,SQLModel,Session
+from sqlmodel import create_engine, SQLModel, Session
+
 app = FastAPI()
 
 # 资源访问
@@ -26,10 +27,8 @@ app.add_middleware(
 )
 # 模板初始化
 jinjaEngine = Jinja2Templates("template")
-#redis
-db = RedisDatabase(host="localhost", port=6379)
-rate = db.rate_limit("speedlimit", limit=5, per=60)  # 每分钟只能调用5次
+
 
 @app.get("/")
 async def index():
-    return 'index'
+    return "index"
