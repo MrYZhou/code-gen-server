e0cf0c773c34096cbfe20a91dee025e8e6c73e4f
larry <*\(^_^)/*>
2022-08-11 17:31:09 +0800

    修改

--- router/generate.py
+++ router/generate.py
@@ -25,10 +25,6 @@ async def index(data: Config):
 
     return {"cacheKey": data.cacheKey, "res": res}
 
-# 预览代码文档根据id
-@router.get("/{id}", tags=["web", "app"])
-async def index(id):
-    pass
 
 # 通过缓存下载
 @router.post("/download")
@@ -48,18 +44,12 @@ async def index(data: Config = Config()):
     
 # 获取数据库的列表
 
-# this can not work,but post api work
-# @router.get("/list",status_code=200)
-# async def index():
-#     with Session(engine) as session:
-#         list = session.exec(select(Config).offset(0).limit(100)).all()
-#         return list 
-@router.post("/list",status_code=200)
+@router.get("/list",status_code=200)
 async def index():
     with Session(engine) as session:
         list = session.exec(select(Config).offset(0).limit(100)).all()
         return list    
-    
+
 
 # 保存生成的配置
 @router.post("/saveConfig")
