3a7fb85e17a85d580efc6d76d336e24adeb4667d
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-11-22 23:16:22 +0800

    数据库初始化，配置文件修改

--- db.py
+++ db.py
@@ -14,6 +14,9 @@ SQLMODEL_ECHO = (
     bool(os.getenv("SQLMODEL_ECHO")) if bool(os.getenv("SQLMODEL_ECHO")) else False
 )
 
-DB_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@host:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+DB_HOST = os.environ.get('DB_HOST') if os.environ.get('DB_HOST') else "127.0.0.1"
+DB_PORT = os.environ.get('DB_PORT') if os.environ.get('DB_PORT') else "3306"
+
+DB_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 print(f'连接地址:{DB_URL}')
 engine = create_engine(DB_URL, echo=SQLMODEL_ECHO)
