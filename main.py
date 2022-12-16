8187794c3fd71c9349debb74cdb1e6e87d00190a
lingen <*\(^_^)/*>
2022-12-16 16:11:57 +0800

    压缩包下载

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 
 from util.system import Init
-
+from util.base import Common
+from fastapi.responses import FileResponse
 app = FastAPI()
 
 Init.do(app)
@@ -11,7 +12,11 @@ Init.do(app)
 @app.get("/")
 async def index():
     return "index"
-
+@app.get("/zipfile")
+async def root():
+    Common.zipfile("./template/java", "./template/java")
+    url = "./template/java.zip"
+    return FileResponse(url, filename="java.zip")
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, workers=1)
     
