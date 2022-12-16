6f99414d9cab06da28833afdf3734ed5a39e84c0
larry <*\(^_^)/*>
2022-12-16 09:55:30 +0800

    增加启动方式入口函数

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from util.system import Init
@@ -10,3 +11,8 @@ Init.do(app)
 @app.get("/")
 async def index():
     return "index"
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, workers=1)
+    
+
