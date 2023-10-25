fb087826afc3149c4bdd1fbee5f84cf5b13c9f22
lg <1762861715@qq.com>
2023-10-25 13:33:34 +0800

    修改

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ async def index():
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=1)
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, workers=1)
