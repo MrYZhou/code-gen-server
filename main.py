04f79e9989fb1007c85b10e2af4a336f04479332
larry <1762861715@qq.com>
2024-01-02 21:08:48 +0800

    优化启动器问题

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import uvicorn
 from fastapi import FastAPI
 
 from util.system import Init
 
-app = None
-
-
-def init():
-    app = FastAPI()
-    Init.do(app)
-
-
-if __name__ == "__main__":
-    init()
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, workers=1)
+app = FastAPI()
+Init.do(app)
