0bbd3d04fba392abfc9f0c00d601a2547f3d5b54
lg <1762861715@qq.com>
2024-01-02 14:11:09 +0800

    只执行一次入口文件

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+
 import uvicorn
 from fastapi import FastAPI
 
 from util.system import Init
 
-app = FastAPI()
-Init.do(app)
+app = None
+
+def init():
+    app = FastAPI()
+    Init.do(app)
 
 if __name__ == "__main__":
+    init()
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, workers=1)
     
