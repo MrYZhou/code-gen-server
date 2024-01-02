f9438332f6423daffc763b72f94f5cd828908f06
larry <1762861715@qq.com>
2024-01-02 19:43:30 +0800

    python-dotenv 移除改用starlette.config

--- db.py
+++ db.py
@@ -1,20 +1,20 @@
 import os
 
-from dotenv import load_dotenv
-from sqlmodel import SQLModel, create_engine
-import records
+from sqlmodel import  create_engine
+from starlette.config import Config
 
+if not os.path.exists(".env"):
+    with open(".env", "w") as f:
+        f.write("DB_HOST=127.0.0.1\n")
+config =  Config('.env')
 
-load_dotenv()
-DB_HOST = os.getenv("DB_HOST") if os.getenv("DB_HOST") else "127.0.0.1"
-DB_PORT = os.getenv("DB_PORT") if os.getenv("DB_PORT") else "3306"
-DB_USER = os.getenv("DB_USER") if os.getenv("DB_USER") else "root"
-DB_PASSWORD = os.getenv("DB_PASSWORD") if os.getenv("DB_PASSWORD") else "123456"
-DB_NAME = os.getenv("DB_NAME") if os.getenv("DB_NAME") else "study"
-DB_DRIVER = os.getenv("DB_DRIVER") if os.getenv("DB_DRIVER") else "mysql+pymysql"
-SQLMODEL_ECHO = bool(os.getenv("SQLMODEL_ECHO"))
-DB_HOST = os.getenv("DB_HOST") if os.getenv("DB_HOST") else "127.0.0.1"
-DB_PORT = os.getenv("DB_PORT") if os.getenv("DB_PORT") else "3306"
+DB_HOST = config.get("DB_HOST",default="127.0.0.1")
+DB_PORT = config.get("DB_PORT",default="3306")
+DB_USER = config.get("DB_USER",default="root")
+DB_PASSWORD = config.get("DB_PASSWORD",default="123456")
+DB_NAME = config.get("DB_NAME",default="study")
+DB_DRIVER = config.get("DB_DRIVER",default="mysql+pymysql")
+SQLMODEL_ECHO = config.get("SQLMODEL_ECHO",default=False)
 
 DB_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 print(f"连接地址 : {DB_URL}")
@@ -22,4 +22,4 @@ print(f"打印sql  : {SQLMODEL_ECHO}")
 engine = create_engine(DB_URL, echo=SQLMODEL_ECHO)
 
 
-rdb = records.Database(f"{DB_URL}")
+# rdb = records.Database(f"{DB_URL}")
