5a669e31059d226e18c778f0b612b4d751d35158
lg <1762861715@qq.com>
2024-01-03 10:35:47 +0800

    提交

--- util/system.py
+++ util/system.py
@@ -6,21 +6,20 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from sqlmodel import SQLModel
 
-from util.base import routeList
 from db import engine
 
 
 # 路由注册
 def initRouter(app: FastAPI):
     # 解析规则:server模块下面的带router字符的文件
-    modulesServer = __import__("server")
-    for root, dirs, files in os.walk(os.path.join(os.getcwd(), "server")):
-        for file in files:
-            if not file.find("router") == -1:
-                file = file.replace(".py", "")
-                parentModule = getattr(modulesServer, os.path.basename(root))
-                module = getattr(parentModule, file)
-                app.include_router(module.router)
+    # modulesServer = __import__("server")
+    # for root, dirs, files in os.walk(os.path.join(os.getcwd(), "server")):
+    #     for file in files:
+    #         if not file.find("router") == -1:
+    #             file = file.replace(".py", "")
+    #             parentModule = getattr(modulesServer, os.path.basename(root))
+    #             module = getattr(parentModule, file)
+    #             app.include_router(module.router)
     # 解析规则:放在router模块下面的文件
     for root, dirs, files in os.walk(os.path.join(os.getcwd(), "router")):
         for file in files:
@@ -30,9 +29,6 @@ def initRouter(app: FastAPI):
             m = importlib.import_module("router." + file)
             app.include_router(m.router)
 
-    # 解析规则:在模板里面手动注册的方式,需要自行导包
-    for route in routeList:
-        app.include_router(route)
 
 
 def initHttp(app: FastAPI):
