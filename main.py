94ee9cfcd936df95308507224508e7ab17c01a68
larry <1762861715@qq.com>
2024-01-03 10:04:57 +0800

    提交

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import  FastAPI
+
 
 from util.system import Init
 
 app = FastAPI()
 Init.do(app)
+
+@app.get("/")
+async def root() -> dict[str, str|int]:
+  return {'code':200,'msg':'success'}
