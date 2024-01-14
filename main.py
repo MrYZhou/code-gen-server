4378dd00f032f553c7ee291f4cb6eac81e70d38d
larry <1762861715@qq.com>
2024-01-15 01:52:00 +0800

    修改启动方式

--- main.py
+++ main.py
@@ -12,3 +12,32 @@ Init.do(app)
 @app.get("/")
 def root():
     return RedirectResponse("https://www.baidu.com")
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    # 输出无日志
+    log_path =os.path.join(os.path.expanduser("~"),"laorm","logfile.log")  # Get the name of the script
+
+    if not os.path.exists(log_path):
+        os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    log_config = {
+        "version": 1,
+        "disable_existing_loggers": True,
+        "handlers": {
+            "file_handler": {
+                "class": "logging.FileHandler",
+                "filename": log_path,
+            },
+        },
+        "root": {
+            "handlers": ["file_handler"],
+            "level": "INFO",
+        },
+    }
+    uvicorn.run(app=app,
+        host="127.0.0.1",
+        port=8000,
+        reload=False,
+        workers=1,
+        # log_config=log_config
+    )
