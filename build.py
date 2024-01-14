f12090ee4bb626f8588751219023a31c27703954
larry <1762861715@qq.com>
2024-01-15 01:29:10 +0800

    构建

--- build.py
+++ build.py
@@ -3,12 +3,11 @@ from PyInstaller import __main__ as pyi
 # 打包快捷设置,避免手动输入命令
 
 params = [
-    "-F",
+    # "-F",
     # '-w',
-    "--hidden-import",
-    "main"
+    # "--hidden-import=main",
     # static目录纳入打包
-    # '--add-data', 'static:static',
+    # '--add-data', 'main:app',
     # 每次打包前清楚build 和 dist目录
     "--clean",
     # 无需用户确认
