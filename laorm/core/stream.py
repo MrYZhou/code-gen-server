e66ca53b2e3d1273d15cfa7d052cd30ca0be499a
larry <1762861715@qq.com>
2024-01-14 21:49:44 +0800

    调整

--- laorm/core/stream.py
+++ laorm/core/stream.py
@@ -2,37 +2,6 @@ from typing import TypeVar
 from abc import ABCMeta
 from laorm.core.PPA import PPA
 
-# class PPA:
-#     _instance = None
-#     pool = None
-
-#     @classmethod
-#     async def startup(cls):
-#         cls.pool = await aiomysql.create_pool(**cls.startup_params)
-
-#     @classmethod
-#     async def shutdown(cls):
-#         if cls.pool is not None:
-#             cls.pool.close()
-#             await cls.pool.wait_closed()
-
-#     @classmethod
-#     async def exec(cls, sql: str, params: Union[Dict[str, any], tuple, list] = None):
-      
-#         # sql注入攻击过滤处理
-#         sql = sql.replace("?", "%s")
-        
-#         if isinstance(params, (dict)):
-#             # 参数化查询（使用字典）,转元组
-#             sql = sql.format_map(dict.fromkeys(params.keys(),'%s'))
-#             params=tuple(params.values())
-
-#         async with cls.pool.acquire() as conn:
-#             async with conn.cursor(aiomysql.DictCursor) as cur:
-#                 await cur.execute(sql,params)
-#                 return await cur.fetchall()
-
-
 class SqlStateMachine:
     def __init__(self,*args):
         self.states = ['INITIAL', 'SELECT', 'FROM', 'WHERE', 'GROUP_BY', 'HAVING', 'ORDER_BY', 'FINAL']
--- start.py
+++ start.py
@@ -1,29 +1,29 @@
 # 使用idea和打包使用
 # vscode默认可以在main.py按f5启动
 
-import os
 import uvicorn
 
 if __name__ == "__main__":
+    # 输出无日志 
+    # log_path =os.path.join(os.path.expanduser("~"),"laorm","logfile.log")  # Get the name of the script
     
-    log_path =os.path.join(os.path.expanduser("~"),"laorm","logfile.log")  # Get the name of the script
-    
-    if not os.path.exists(log_path):
-        os.makedirs(os.path.dirname(log_path), exist_ok=True)
-    log_config = {
-        "version": 1,
-        "disable_existing_loggers": True,
-        "handlers": {
-            "file_handler": {
-                "class": "logging.FileHandler",
-                "filename": log_path,
-            },
-        },
-        "root": {
-            "handlers": ["file_handler"],
-            "level": "INFO",
-        },
-    }
+    # if not os.path.exists(log_path):
+    #     os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    # log_config = {
+    #     "version": 1,
+    #     "disable_existing_loggers": True,
+    #     "handlers": {
+    #         "file_handler": {
+    #             "class": "logging.FileHandler",
+    #             "filename": log_path,
+    #         },
+    #     },
+    #     "root": {
+    #         "handlers": ["file_handler"],
+    #         "level": "INFO",
+    #     },
+    # }
+    log_config={}
     uvicorn.run(
         app="main:app",
         host="127.0.0.1",
