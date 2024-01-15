999262d4f43df10e6746ceeb2bba139a061920c7
lg <1762861715@qq.com>
2024-01-15 10:06:19 +0800

    打包配置

--- build.py
+++ build.py
@@ -1,19 +1,22 @@
-import os
 from PyInstaller import __main__ as pyi
 
 # 打包快捷设置,避免手动输入命令
 
 params = [
-    # "-F",
-    # '-w',
+    # "-F", #  是否单文件
+    # '-w', #是否隐藏控制台
     "--hidden-import=aiomysql",
-    # static目录纳入打包
-    # '--collect-submodules', 'util',
-    # '--collect-submodules', 'router',
-    # '--collect-submodules', 'server',
-    # '--add-data', 'util;util',
-    # '--add-data', 'router;router',
-    # '--add-data', 'server;server',
+    "--hidden-import=nanoid",
+    "--hidden-import=sqlmodel",
+    "--hidden-import=pysqlite2",
+    "--hidden-import=MySQLdb",
+    "--hidden-import=laorm.core.stream",
+    "--hidden-import=fastapi.templating",
+    
+    # 额外目录纳入打包
+    '--add-data', 'util;util',
+    '--add-data', 'router;router',
+    '--add-data', 'server;server',
     # 每次打包前清楚build 和 dist目录
     # "--clean",
     # 无需用户确认
