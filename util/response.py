58bf272dbee312c6134f9c9fef2ac25b9476a28d
larry <1762861715@qq.com>
2024-02-18 15:18:50 +0800

    响应错误

--- util/response.py
+++ util/response.py
@@ -34,11 +34,11 @@ class AppResult:
       if len(args)==0:
         return AppResult(code=400, msg="Fail")
       if len(args)==1:
-        if isinstance(args[1], str):
-          return AppResult(code=400, msg=args[1])
-        return AppResult(code=400,data=args[1], msg="Fail")
+        if isinstance(args[0], str):
+          return AppResult(code=400, msg=args[0])
+        return AppResult(code=400,data=args[0], msg="Fail")
       if len(args)==2:
-         return AppResult(code=400, data=args[1], msg=args[2])
+         return AppResult(code=400, data=args[0], msg=args[1])
    
 
     @classmethod
