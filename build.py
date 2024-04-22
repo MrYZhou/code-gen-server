c47b3f804d1b420827b5831334f5992d1bae42b4
larry <1762861715@qq.com>
2024-04-22 15:33:27 +0800

    打包

--- build.py
+++ build.py
@@ -4,12 +4,15 @@ from PyInstaller import __main__ as pyi
 
 params = [
     "-F",  #  是否单文件
-    # '-w', # 是否隐藏控制台-w代表无,默认有
+    # "-w",  # 是否隐藏控制台-w代表无,默认有
     "--hidden-import=fastapi.templating",
+    "--hidden-import=nanoid",
+    "--hidden-import=faker",
     # 额外目录纳入打包
     "--add-data=util;util",
     "--add-data=router;router",
     "--add-data=server;server",
+    "--add-data=models;models",
     # 指定输出目录
     "--distpath=build",
     # 无需用户确认
