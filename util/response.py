6024386f7d6fde804d240f3f2109d17e01d5d9f5
larry <1762861715@qq.com>
2024-11-21 14:06:01 +0800

    模型封装

--- util/response.py
+++ util/response.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Any, List
+
+from fastapi.responses import JSONResponse
 
 
 @dataclass
@@ -16,36 +18,37 @@ class PageListVO:
 
 
 @dataclass
+@dataclass
 class AppResult:
     code: int
     msg: str
-    data: object = None
+    data: Any = None
 
     @classmethod
     def success(cls, *args):
         if len(args) == 0:
-            return {"code": 200, "msg": "Success"}
+            return JSONResponse(status_code=200, content={"code": 200, "msg": "Success"})
         if len(args) == 1:
             if isinstance(args[0], str):
-                return {"code": 200, "msg": args[0]}
-            return {"code": 200, "data": args[0], "msg": "Success"}
+                return JSONResponse(status_code=200, content={"code": 200, "msg": args[0]})
+            return JSONResponse(status_code=200, content={"code": 200, "data": args[0], "msg": "Success"})
         if len(args) == 2:
-            return {"code": 200, "data": args[0], "msg": args[1]}
+            return JSONResponse(status_code=200, content={"code": 200, "data": args[0], "msg": args[1]})
 
     @classmethod
     def fail(cls, *args):
         if len(args) == 0:
-            return AppResult(code=400, msg="Fail")
+            return JSONResponse(status_code=400, content={"code": 400, "msg": "Fail"})
         if len(args) == 1:
             if isinstance(args[0], str):
-                return {"code": 400, "msg": args[0]}
-            return {"code": 400, "data": args[0], "msg": "Fail"}
+                return JSONResponse(status_code=400, content={"code": 400, "msg": args[0]})
+            return JSONResponse(status_code=400, content={"code": 400, "data": args[0], "msg": "Fail"})
         if len(args) == 2:
             if isinstance(args[0], int):
-                return {"code": args[0], "msg": args[1]}
-            return {"code": 400, "data": args[0], "msg": args[1]}
+                return JSONResponse(status_code=args[0], content={"code": args[0], "msg": args[1]})
+            return JSONResponse(status_code=400, content={"code": 400, "data": args[0], "msg": args[1]})
 
     @classmethod
     def page(cls, list: List, pagination: PaginationVO):
         vo = PageListVO(list=list, pagination=pagination)
-        return {"code": 200, "data": vo, "msg": "Success"}
+        return JSONResponse(status_code=200, content={"code": 200, "data": vo, "msg": "Success"})
